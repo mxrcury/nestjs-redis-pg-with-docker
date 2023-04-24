087d6ec07bce7839b131cfa4c8d95fd3533be4dc
mxrcury <demetrgonchar@gmail.com>
2023-04-24 07:48:52 +0300

    added gitignore and script for seeding

--- dist/database/seeds/films.seed.d.ts
+++
@@ -1,4 +0,0 @@
-import { More, OnSeederInit, SeederModuleOptions } from "nestjs-sequelize-seeder";
-export declare class FilmSeed implements OnSeederInit {
-    run(options: SeederModuleOptions): More[];
-}
